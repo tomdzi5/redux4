7bf15ae6c0347382413dc0700378bb29e3fabde3
Tomasz_Dziedzic <tomasz.dziedzic@beefee.co.uk>
2017-10-05 17:03:08 +0200

    aaa

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+
+class Hello extends
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
